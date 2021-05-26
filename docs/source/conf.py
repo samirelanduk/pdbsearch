5ab77908cb829b2ced00b38fb2eb03273e1c5b70
Sam Ireland <sam.ireland.uk@gmail.com>
2021-05-26 23:17:45 +0100

    Start v0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Sam Ireland"
 author = "Sam Ireland"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
