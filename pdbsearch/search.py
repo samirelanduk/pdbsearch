e49aa3c8734e1675faa8dd1bd893189a50e3f583
Sam Ireland <mail@samireland.com>
2025-11-01 15:23:46 +0000

    Text service operators

--- pdbsearch/search.py
+++ pdbsearch/search.py
@@ -1,5 +1,4 @@
 import requests
-from typing import Any, Callable
 
 SEARCH_URL = "https://search.rcsb.org/rcsbsearch/v2/query"
 
@@ -9,33 +8,53 @@ def search(return_type, ids_only=False, **kwargs):
     }
     if len(kwargs) == 1:
         key, value = kwargs.popitem()
+        if key.endswith("__in"):
+            key = key.replace("__in", "")
+            operator = "in"
+        elif key.endswith("__gt"):
+            key = key.replace("__gt", "")
+            operator = "greater"
+        elif key.endswith("__gte"):
+            key = key.replace("__gte", "")
+            operator = "greater_or_equal"
+        elif key.endswith("__lt"):
+            key = key.replace("__lt", "")
+            operator = "less"
+        elif key.endswith("__lte"):
+            key = key.replace("__lte", "")
+            operator = "less_or_equal"
+        elif key.endswith("__eq"):
+            key = key.replace("__eq", "")
+            operator = "equals"
+        elif key.endswith("__exists"):
+            key = key.replace("__exists", "")
+            operator = "exists"
+        elif key.endswith("__contains_words"):
+            key = key.replace("__contains_words", "")
+            operator = "contains_words"
+        elif key.endswith("__contains_phrase"):
+            key = key.replace("__contains_phrase", "")
+            operator = "contains_phrase"
+        else:
+            operator = "exact_match"
         key = key.replace("__", ".")
         query["query"] = {
             "type": "terminal",
             "service": "text",
             "parameters": {
                 "attribute": key,
-                "operator": "exact_match",
+                "operator": operator,
                 "value": value
             }
         }
+        if operator == "exists":
+            del query["query"]["parameters"]["value"]
     response = requests.post(SEARCH_URL, json=query)
     result = response.json()
     if ids_only:
         return [r["identifier"] for r in result["result_set"]]
     else:
         return result
-
-
-
-function_names = {
-    "entry": "entries",
-    "polymer_entity": "polymer_entities",
-    "non_polymer_entity": "non_polymer_entities",
-    "polymer_instance": "polymer_instances",
-    "assembly": "assemblies",
-    "mol_definition": "mol_definitions",
-}
 
 
 
