a8dc1c031776007e1e6c6946b4c20ea3c4b4f918
Sam Ireland <mail@samireland.com>
2025-11-01 15:06:41 +0000

    Text service exact match

--- pdbsearch/search.py
+++ pdbsearch/search.py
@@ -2,33 +2,45 @@ import requests
 
 SEARCH_URL = "https://search.rcsb.org/rcsbsearch/v2/query"
 
-def search(return_type):
+def search(return_type, **kwargs):
     query = {
         "return_type": return_type,
     }
+    if len(kwargs) == 1:
+        key, value = kwargs.popitem()
+        key = key.replace("__", ".")
+        query["query"] = {
+            "type": "terminal",
+            "service": "text",
+            "parameters": {
+                "attribute": key,
+                "operator": "exact_match",
+                "value": value
+            }
+        }
     response = requests.post(SEARCH_URL, json=query)
     return response.json()
 
 
-def entries():
-    return search("entry")
+def entries(**kwargs):
+    return search("entry", **kwargs)
 
 
-def polymer_entities():
-    return search("polymer_entity")
+def polymer_entities(**kwargs):
+    return search("polymer_entity", **kwargs)
 
 
-def non_polymer_entities():
-    return search("non_polymer_entity")
+def non_polymer_entities(**kwargs):
+    return search("non_polymer_entity", **kwargs)
 
 
-def polymer_instances():
-    return search("polymer_instance")
+def polymer_instances(**kwargs):
+    return search("polymer_instance", **kwargs)
 
 
-def assemblies():
-    return search("assembly")
+def assemblies(**kwargs):
+    return search("assembly", **kwargs)
 
 
-def mol_definitions():
-    return search("mol_definition")
+def mol_definitions(**kwargs):
+    return search("mol_definition", **kwargs)
