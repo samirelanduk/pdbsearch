9a1e82ecadbb6f51c79e0f439767a9d52ea8a078
Sam Ireland <mail@samireland.com>
2025-11-01 18:49:30 +0000

    Terminal node execution

--- pdbsearch/models.py
+++ pdbsearch/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from .request import send_request
 
 @dataclass
 class TerminalNode:
@@ -16,3 +17,14 @@ class TerminalNode:
         node = {"type": "terminal", "service": self.service}
         if self.parameters: node["parameters"] = self.parameters
         return node
+    
+
+    def execute(self, return_type, ids_only=False):
+        """Executes the node and returns the result.
+        
+        :param str return_type: the RCSB object type to search for.
+        :param bool ids_only: whether to return only the result identifiers.
+        :rtype: ``list`` or ``dict``"""
+
+        request = {"return_type": return_type, "query": self.serialize()}
+        return send_request(request, ids_only=ids_only)
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 from pdbsearch.models import TerminalNode
 
 class TerminalNodeCreationTests(TestCase):
@@ -20,3 +21,32 @@ class TerminalNodeSerializationTests(TestCase):
     def test_can_serialize_with_parameters(self):
         node = TerminalNode(service="service_name", parameters={"key": "value"})
         self.assertEqual(node.serialize(), {"type": "terminal", "service": "service_name", "parameters": {"key": "value"}})
+
+
+
+class TerminalNodeExecutionTests(TestCase):
+
+    @patch("pdbsearch.models.TerminalNode.serialize")
+    @patch("pdbsearch.models.send_request")
+    def test_can_execute_terminal_node(self, mock_send_request, mock_serialize):
+        node = TerminalNode(service="service_name", parameters={"key": "value"})
+        result = node.execute(return_type="entry")
+        self.assertEqual(result, mock_send_request.return_value)
+        mock_send_request.assert_called_once_with(
+            {"return_type": "entry", "query": mock_serialize.return_value},
+            ids_only=False
+        )
+        mock_serialize.assert_called_once_with()
+    
+
+    @patch("pdbsearch.models.TerminalNode.serialize")
+    @patch("pdbsearch.models.send_request")
+    def test_can_execute_terminal_node_ids_only(self, mock_send_request, mock_serialize):
+        node = TerminalNode(service="service_name", parameters={"key": "value"})
+        result = node.execute(return_type="entry", ids_only=True)
+        self.assertEqual(result, mock_send_request.return_value)
+        mock_send_request.assert_called_once_with(
+            {"return_type": "entry", "query": mock_serialize.return_value},
+            ids_only=True
+        )
+        mock_serialize.assert_called_once_with()
