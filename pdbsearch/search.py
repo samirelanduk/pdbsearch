95d3ceab0c4a2f59cf3bf23b240d95406d1d2d42
Sam Ireland <mail@samireland.com>
2025-11-01 15:35:32 +0000

    Text service

--- pdbsearch/search.py
+++ pdbsearch/search.py
@@ -2,62 +2,83 @@ import requests
 
 SEARCH_URL = "https://search.rcsb.org/rcsbsearch/v2/query"
 
-def search(return_type, ids_only=False, **kwargs):
+def search(return_type, ids_only=False, text=None, **kwargs):
     query = {
         "return_type": return_type,
     }
-    if len(kwargs) == 1:
-        key, value = kwargs.popitem()
-        if key.endswith("__in"):
-            key = key.replace("__in", "")
-            operator = "in"
-        elif key.endswith("__gt"):
-            key = key.replace("__gt", "")
-            operator = "greater"
-        elif key.endswith("__gte"):
-            key = key.replace("__gte", "")
-            operator = "greater_or_equal"
-        elif key.endswith("__lt"):
-            key = key.replace("__lt", "")
-            operator = "less"
-        elif key.endswith("__lte"):
-            key = key.replace("__lte", "")
-            operator = "less_or_equal"
-        elif key.endswith("__eq"):
-            key = key.replace("__eq", "")
-            operator = "equals"
-        elif key.endswith("__exists"):
-            key = key.replace("__exists", "")
-            operator = "exists"
-        elif key.endswith("__contains_words"):
-            key = key.replace("__contains_words", "")
-            operator = "contains_words"
-        elif key.endswith("__contains_phrase"):
-            key = key.replace("__contains_phrase", "")
-            operator = "contains_phrase"
-        else:
-            operator = "exact_match"
-        negation = False
-        if key.endswith("__not"):
-            key = key.replace("__not", "")
-            negation = True
-        key = key.replace("__", ".")
-        query["query"] = {
+
+    nodes = []
+    if text:
+        nodes.append({
             "type": "terminal",
-            "service": "text",
+            "service": "full_text",
             "parameters": {
-                "attribute": key,
-                "operator": operator,
-                "value": value
+                "value": text
             }
+        })
+    if kwargs:
+        for key, value in kwargs.items():
+            if key.endswith("__in"):
+                key = key.replace("__in", "")
+                operator = "in"
+            elif key.endswith("__gt"):
+                key = key.replace("__gt", "")
+                operator = "greater"
+            elif key.endswith("__gte"):
+                key = key.replace("__gte", "")
+                operator = "greater_or_equal"
+            elif key.endswith("__lt"):
+                key = key.replace("__lt", "")
+                operator = "less"
+            elif key.endswith("__lte"):
+                key = key.replace("__lte", "")
+                operator = "less_or_equal"
+            elif key.endswith("__eq"):
+                key = key.replace("__eq", "")
+                operator = "equals"
+            elif key.endswith("__exists"):
+                key = key.replace("__exists", "")
+                operator = "exists"
+            elif key.endswith("__contains_words"):
+                key = key.replace("__contains_words", "")
+                operator = "contains_words"
+            elif key.endswith("__contains_phrase"):
+                key = key.replace("__contains_phrase", "")
+                operator = "contains_phrase"
+            else:
+                operator = "exact_match"
+            negation = False
+            if key.endswith("__not"):
+                key = key.replace("__not", "")
+                negation = True
+            key = key.replace("__", ".")
+            nodes.append({
+                "service": "text",
+                "type": "terminal",
+                "parameters": {
+                    "attribute": key,
+                    "operator": operator,
+                    "value": value
+                }
+            })
+            if operator == "exists":
+                del nodes[-1]["parameters"]["value"]
+                if value == False:
+                    nodes[-1]["parameters"]["negation"] = True
+            if negation:
+                nodes[-1]["parameters"]["negation"] = True
+    
+    if len(nodes) == 1:
+        query["query"] = nodes[0]
+    elif len(nodes) > 1:
+        query["query"] = {
+            "type": "group",
+            "logical_operator": "and",
+            "nodes": nodes
         }
-        if operator == "exists":
-            del query["query"]["parameters"]["value"]
-            if value == False:
-                query["query"]["parameters"]["negation"] = True
-        if negation:
-            query["query"]["parameters"]["negation"] = True
+        
     response = requests.post(SEARCH_URL, json=query)
+    if response.status_code == 204: return None
     result = response.json()
     if ids_only:
         return [r["identifier"] for r in result["result_set"]]
